7fa6ff83a9f6609e1f37252006811a39189fb957
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-02-04 22:42:40 +0900

    Summer: Storyboard content link & UIImagePickerViewController using

--- MainViewController.swift
+++ MainViewController.swift
@@ -8,9 +8,38 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.imageView.image = nil
+    }
+    
+    @IBAction func presentAlbum() {
+        let controller = UIImagePickerController()
+        controller.sourceType = .photoLibrary
+        
+        controller.delegate = self
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func presentCamera() {
+        print("camera")
+    }
+    
+    @IBAction func saveImage() {
+        print("save")
     }
 }
 
+extension MainViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+        print("did cancel")
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("did finish")
+    }
+}
